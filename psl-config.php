391d69a7f4fec653d1d85e9077e8246ef2eebbb4
Rasmus Lauridsen <r.h.lauridsen@hotmail.dk>
2020-10-21 09:25:42 +0200

    Init commit

---
+++ psl-config.php
@@ -0,0 +1,7 @@
+<?php
+define("HOST", "116.202.245.110");
+define("USER", "rasmusla_3iprogrammering");
+define("PASSWORD", "GwSAX6Cf0jZFLOj70Vv5Iyage29mjI8Mn");
+define("DATABASE", "rasmusla_3iprogrammering");
+
+
