bf026e5da8016282fee88a8cc69cb1a3817a7a25
Rasmus Lauridsen <r.h.lauridsen@hotmail.dk>
2020-10-22 13:18:34 +0200

    Delete psl-config.php

--- psl-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("HOST", "116.202.245.110");
-define("USER", "rasmusla_3iprogrammering");
-define("PASSWORD", "GwSAX6Cf0jZFLOj70Vv5Iyage29mjI8Mn");
-define("DATABASE", "rasmusla_3iprogrammering");
-
-
